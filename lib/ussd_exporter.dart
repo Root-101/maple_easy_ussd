80582c1d90ba5ddfadb494d0b1bfbcdb5468d14f
Jesus Hdez <jhernandezb96@gmail.com>
2022-09-15 16:02:07 -0400

    Refactor core. New 'app' layer added for config with single project as single product and an maple module

--- lib/ussd_exporter.dart
+++ lib/ussd_exporter.dart
@@ -1,8 +1,9 @@
 library ussd_exporter;
 
 export 'src/actions/actions_exporter.dart';
-export 'src/app/ussd_app_exporter.dart';
+export 'src/core/ussd_app_exporter.dart';
 export 'src/repo/ussd_repo_exporter.dart';
 export 'src/ui/ussd_ui_exporter.dart';
+export 'src/app/ussd_app_exporter.dart';
 
 export 'objectbox.g.dart';
