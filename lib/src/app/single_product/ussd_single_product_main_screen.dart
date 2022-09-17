ae1fc6b05c60287a2ddd02cb08eb60d35dc5d085
Jesus Hdez <jhernandezb96@gmail.com>
2022-09-16 21:51:24 -0400

    +theme switcher

--- lib/src/app/single_product/ussd_single_product_main_screen.dart
+++ lib/src/app/single_product/ussd_single_product_main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_ussd/ussd_exporter.dart';
 import 'package:get/get.dart';
@@ -14,6 +15,16 @@ class USSDSingleProductMainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ThemeSwitchingArea(
+      child: Builder(
+        builder: (context) {
+          return _scaffold();
+        },
+      ),
+    );
+  }
+
+  Widget _scaffold() {
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -21,15 +32,50 @@ class USSDSingleProductMainScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-              onPressed: () {
-                _introScreenController.resetOpenApp();
-              },
-              icon: Tooltip(
-                message: "Muestra de nuevo el introduction screen",
-                child: Icon(
-                  Icons.lightbulb_circle_outlined,
+            onPressed: () {
+              _introScreenController.resetOpenApp();
+            },
+            icon: Tooltip(
+              message: "Muestra de nuevo el introduction screen",
+              child: Icon(
+                Icons.lightbulb_circle_outlined,
+              ),
+            ),
+          ),
+          ThemeSwitcher(
+            clipper: const ThemeSwitcherCircleClipper(),
+            builder: (context) {
+              Brightness br =
+                  ThemeModelInheritedNotifier.of(context).theme.brightness;
+              return IconButton(
+                onPressed: () {
+                  ThemeData l = ThemeData(
+                    brightness: Brightness.light,
+                    primarySwatch: Colors.orange,
+                  );
+                  ThemeData d = ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.brown,
+                  );
+
+                  if (br == Brightness.light) {
+                    ThemeSwitcher.of(context).changeTheme(
+                      theme: d,
+                    );
+                  } else {
+                    ThemeSwitcher.of(context).changeTheme(
+                      theme: l,
+                    );
+                  }
+                },
+                icon: Icon(
+                  br == Brightness.light
+                      ? Icons.format_paint_outlined
+                      : Icons.format_paint,
                 ),
-              ))
+              );
+            },
+          ),
         ],
       ),
       //body: USSDMainBody(),
