5cfd7f919f101f648f8cf240407b551b8621d3b7
Aidyl98 <jgarciaalbalah@gmail.com>
2022-12-01 14:56:39 -0500

    + ColorsTheme

---
+++ lib/src/app/theme/colors_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// All the colors that the app is going to have.
+class ColorsTheme {
+  // Primary color range.
+  static const Color primary = Color(0xFFB71C1C);
+  static const Color primaryDark = Color(0xFF7F0000);
+  static const Color primaryLight = Color(0xFFF05545);
+  static const Color primaryExtraLight = Color(0xFFE57373);
+
+  // The background color.
+  static const Color background = gray;
+
+  // Gray colors range.
+  static const Color grayDark = Color(0xFF7A808A);
+  static const Color grayNeutral = Color(0xFFA4ACC1);
+  static const Color gray = Color(0xFFD5D6D9);
+  static const Color grayMid = Color(0xFFEEF0F4);
+  static const Color grayLight = Color(0xFFF8F8F8);
+}
