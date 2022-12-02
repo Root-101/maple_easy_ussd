eeec9571a884a38e37d346f9d233af7f71b6ba85
Aidyl98 <jgarciaalbalah@gmail.com>
2022-12-02 18:45:14 -0500

    exporter updated

--- lib/src/features/favorites/view/ussd_favorites_view_exporter.dart
+++ lib/src/features/favorites/view/ussd_favorites_view_exporter.dart
@@ -4,3 +4,4 @@ export 'controller/ussd_favorites_controller.dart';
 export 'controller_impl/ussd_favorites_controller_impl.dart';
 export 'widgets/ussd_favorite_button.dart';
 export 'widgets/ussd_favorites_tab.dart';
+export 'widgets/text_container.dart';
