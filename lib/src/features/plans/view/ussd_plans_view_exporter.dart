fe98c546054e1349a49ddff044ca8fb54ecddc40
Aidyl98 <jgarciaalbalah@gmail.com>
2022-12-01 19:50:35 -0500

    update exporters

--- lib/src/features/plans/view/ussd_plans_view_exporter.dart
+++ lib/src/features/plans/view/ussd_plans_view_exporter.dart
@@ -3,8 +3,11 @@ library ussd_plans_view_exporter;
 export 'controller/ussd_plans_controller.dart';
 export 'controller_impl/ussd_plans_controller_impl.dart';
 
-export 'widgets/ussd_plan_item.dart';
-export 'widgets/ussd_plans_widgets.dart';
-export 'widgets/ussd_plans_tab.dart';
+export 'screens/ussd_plans_tab.dart';
 
 export 'model/ussd_plans_group_model.dart';
+export 'model/ussd_plan_item_model.dart';
+
+export 'widgets/plans_widgets_exporter.dart';
+
+export 'utils/plans_groups_data.dart';
