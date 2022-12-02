4125696518c7df09d73b2b3258954fb979df7ce8
Aidyl98 <jgarciaalbalah@gmail.com>
2022-12-02 18:45:53 -0500

    USSDFavoritesTab: + ColorsTheme and TextContainer

--- lib/src/features/favorites/view/widgets/ussd_favorites_tab.dart
+++ lib/src/features/favorites/view/widgets/ussd_favorites_tab.dart
@@ -1,6 +1,6 @@
+import 'package:easy_ussd/src/app/theme/colors_theme.dart';
 import 'package:easy_ussd/ussd_exporter.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:flutter_animator/flutter_animator.dart';
@@ -18,9 +18,12 @@ class USSDFavoritesTab {
         ),
       );
     }),
+    // Title for the bar item.
     title: ("Favoritos"),
-    activeColorPrimary: Colors.redAccent,
-    inactiveColorPrimary: CupertinoColors.systemGrey,
+    // Color for the current selected item in the navigation bar
+    activeColorPrimary: ColorsTheme.primary,
+    // Color for the unselected item(s) in the navigation bar.
+    inactiveColorPrimary: ColorsTheme.grayDark,
   );
 }
 
@@ -32,14 +35,10 @@ class USSDFavoritesTabBody extends GetView<USSDFavoritesController> {
     return GetBuilder<USSDFavoritesController>(
       builder: (_) {
         List<USSDFavoritesCodes> favoritesConsult =
-        controller.findFavorites_Consults();
+            controller.findFavorites_Consults();
 
         List<USSDFavoritesCodes> favoritesPlans =
-        controller.findFavorites_Plans();
-
-        List<USSDFavoritesCodes> all = [];
-        all.addAll(favoritesConsult);
-        all.addAll(favoritesPlans);
+            controller.findFavorites_Plans();
 
         return CustomScrollView(
           slivers: [
@@ -48,15 +47,44 @@ class USSDFavoritesTabBody extends GetView<USSDFavoritesController> {
             ),
             favoritesConsult.isEmpty && favoritesPlans.isEmpty
                 ? SliverFillRemaining(
-              child: Center(
-                child: Text('No hay favoritos'),
-              ),
-            )
+                    child: Center(
+                      child: Text('No hay favoritos'),
+                    ),
+                  )
                 : SliverList(
-              delegate: SliverChildListDelegate.fixed(
-                all.map((child) => child.widget).toList(),
-              ),
-            ),
+                    delegate: SliverChildListDelegate.fixed(
+                      [
+                        if (favoritesConsult.isNotEmpty)
+                          // Builds a Widget similar to a [TextArea]
+                          // but insted of a text, the area will hold a widget.
+                          TextContainer(
+                            textTitle: "Consultas",
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              // So de list can't be scrolled.
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: favoritesConsult.length,
+                              itemBuilder: (context, item) =>
+                                  favoritesConsult[item].widget,
+                            ),
+                          ),
+                        if (favoritesPlans.isNotEmpty)
+                          // Builds a Widget similar to a [TextArea]
+                          // but insted of a text, the area will hold a widget.
+                          TextContainer(
+                            textTitle: "Planes",
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              // So de list can't be scrolled.
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: favoritesPlans.length,
+                              itemBuilder: (context, item) =>
+                                  favoritesPlans[item].widget,
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
           ],
         );
       },
