7eb48070831fe6c665181d15580de2c300423f87
Aidyl98 <jgarciaalbalah@gmail.com>
2022-12-01 19:49:44 -0500

    USSDPlansGroupsModel: + USSDPlanItemModel

--- lib/src/features/plans/view/model/ussd_plans_group_model.dart
+++ lib/src/features/plans/view/model/ussd_plans_group_model.dart
@@ -1,15 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:easy_ussd/src/features/plans/ussd_plans_exporter.dart';
 import 'package:easy_ussd/ussd_exporter.dart';
 
 class USSDPlansGroupsModel {
   String groupKey;
-  StatelessWidget title;
-  List<USSDPlanItem> childs;
+  String title;
+  List<USSDPlanItemModel> childs;
 
   USSDPlansGroupsModel({
     required this.groupKey,
     required this.title,
     required this.childs,
   });
-
 }
